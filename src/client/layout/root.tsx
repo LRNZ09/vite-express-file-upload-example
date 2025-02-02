60a7321a6de58395a857eba309a575a5391cf9c6
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2025-02-03 00:51:46 +0100

    feat(client): add root layout component

---
+++ src/client/layout/root.tsx
@@ -0,0 +1,5 @@
+import { type PropsWithChildren } from 'react';
+
+export const RootLayout = ({ children }: PropsWithChildren) => (
+    <main className="relative isolate h-dvh">{children}</main>
+);
