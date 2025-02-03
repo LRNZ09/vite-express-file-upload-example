82910af47ab6cd67e9057730e4497c86ebf42d06
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2025-02-03 11:16:40 +0100

    build(client): add testing library related pkgs

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
     test: {
         env: {
             NODE_ENV: 'test',
         },
+        setupFiles: [
+            './vitest-testing-library-setup.ts', // https://testing-library.com/docs/react-testing-library/setup#auto-cleanup-in-vitest
+        ],
     },
 });
