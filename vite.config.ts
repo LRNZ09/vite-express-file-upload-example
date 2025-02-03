690cdb05d6e31bf238b9d0f3a3483b98159ba6c7
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2025-02-03 01:32:56 +0100

    refactor(client): add file upload button uploading debounce

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,15 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { defineConfig } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
     test: {
         env: {
             NODE_ENV: 'test',
         },
+        setupFiles: [
+            './vitest-fast-check-setup.ts', // https://fast-check.dev/docs/configuration/global-settings/#vitest
+            './vitest-testing-library-setup.ts', // https://testing-library.com/docs/react-testing-library/setup#auto-cleanup-in-vitest
+        ],
     },
 });
