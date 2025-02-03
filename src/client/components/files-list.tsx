39926c0f8b9f2b71340c569733b7027dd24c49d3
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2025-02-03 18:42:20 +0100

    feat(client): add file list to home page card

---
+++ src/client/components/files-list.tsx
@@ -0,0 +1,21 @@
+import { useFileList } from '@/hooks/use-file-list';
+
+import { FileListTable } from './file-list-table';
+import { Skeleton } from './ui/skeleton';
+
+export const FileList = () => {
+    const { data } = useFileList();
+
+    // TODO: Add better loading and error handling
+
+    if (data === undefined) {
+        return (
+            <div className="flex flex-row gap-4 items-center justify-center">
+                <Skeleton className="h-4 w-3/5" />
+                <Skeleton className="h-4 w-2/5" />
+            </div>
+        );
+    }
+
+    return <FileListTable caption="A list of your uploaded files." files={data.files} />;
+};
