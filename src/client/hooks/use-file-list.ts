1ec2eaf151778e3500fca009481c9f05b45b5070
Lorenzo Pieri <13508373+LRNZ09@users.noreply.github.com>
2025-02-03 18:20:47 +0100

    feat(client): add use file list hook

---
+++ src/client/hooks/use-file-list.ts
@@ -0,0 +1,10 @@
+import useSWR, { type Fetcher } from 'swr';
+
+import { fetcher } from '@/helpers/fetcher';
+
+interface FileListResponse {
+    readonly files: readonly { readonly name: string; readonly size: number }[];
+}
+
+// TODO: Use a typed API to share the response type between the server and client
+export const useFileList = () => useSWR('/api/files', fetcher as Fetcher<FileListResponse>);
